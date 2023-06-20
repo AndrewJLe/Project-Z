ffc27ef4bc216838a569f8d7edd1e40c0397dd8d
Jin Cai <jinruncai@gmail.com>
2023-06-19 17:49:24 -0700

    __Restart Demo Button__

--- src/components/MatterCanvas.tsx
+++ src/components/MatterCanvas.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import useInitMatter from '../hooks/useInitMatter';
 
 function MatterCanvas(): React.ReactElement {
-    useInitMatter("matter-canvas");
+    const canvasRef = React.useRef<HTMLDivElement>(null);
+
+    const { restart } = useInitMatter(canvasRef);
 
     return (
-        <div id="matter-canvas">
-
+        <div>
+            <div id="matter-canvas" ref={canvasRef}></div>
+            <button onClick={restart}>Restart</button>
         </div>
     );
 };
